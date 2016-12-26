ac3895d2f62f1485b0192273ed152c9a8754ecfe
Shunsuke Taguchi <shunsuke.taguchi.st@gmail.com>
2016-12-26 18:38:21 +0900

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def full_title(page_title = '')
 		base_title = "Ruby on Rails Tutorial Sample App"
-		if page_title.emplty?
+		if page_title.empty?
 			base_title
 		else
 			page_title + " | " + base_title

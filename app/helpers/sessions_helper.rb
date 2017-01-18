7d3d2ba776d1e8899a871fc04db6b5cd83cb94b5
Shunsuke Taguchi <shunsuke.taguchi.st@gmail.com>
2017-01-18 18:22:59 +0900

    Finish user edit, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,21 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	#永続セッションとしてユーザーを記憶する
+	def remember(user)
+		user.remember
+		cookies.permanen.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	#渡されたユーザーがログイン済みユーザーであればtrueを返す
+	def current_user?(user)
+		user == current_user
+	end
+
+	#記憶トークンに対応するユーザーを返す
+	
+
 	#現在ログイン中のユーザーを返す(いる場合)
 	def current_user
 		@current_user ||= User.find_by(id:session[:user_id])
@@ -18,6 +33,17 @@ module SessionsHelper
 	def log_out
 		session.delete(:user_id)
 		@current_user = nil
+	end
+
+	#記憶したURL
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	#アクセスしようとしたURLを覚えておく
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
 	end
 
 end

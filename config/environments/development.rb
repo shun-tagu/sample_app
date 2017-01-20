ab8b41ecb8aadb2d122f8ade60eee4773929a9bb
Shunsuke Taguchi <shunsuke.taguchi.st@gmail.com>
2017-01-20 11:24:50 +0900

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '0.0.0.0:3000' 
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
